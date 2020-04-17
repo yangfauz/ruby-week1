29d6cffb7c6a9ecd3f91206b0759a78f3ab70350
Yayang Fauzi <fauzi@Yayangs-MacBook-Pro.local>
2020-04-17 13:26:45 +0700

    ready to production

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,56 @@
+class TodosController < ApplicationController
+	before_action :set_todo, only: [:edit, :update, :show, :destroy]
+
+	def new
+		@todo = Todo.new
+	end
+
+	def create
+		@todo = Todo.new(todo_params)
+		if @todo.save
+			flash[:notice] = "create sukses"
+			redirect_to todo_path(@todo)
+		else
+			render 'new'
+		end
+	end
+
+	def show
+		
+	end
+
+	def edit
+		
+	end
+
+	def update
+		
+		if @todo.update(todo_params)
+			flash[:notice] = "berhasil diupdate"
+			redirect_to todo_path(@todo)
+		else
+			render 'edit'
+		end
+	end
+
+	def index
+		@todos = Todo.all
+	end
+
+	def destroy
+		
+		@todo.destroy
+		flash[:notice] = "berhasil dihapus"
+		redirect_to todos_path
+	end
+
+	private
+
+		def set_todo
+			@todo = Todo.find(params[:id])
+		end
+
+		def todo_params
+			params.require(:todo).permit(:name, :description)
+		end
+end
---
+++ app/views/pages/about.html.erb
@@ -0,0 +1,1 @@
+<h1>Ini About</h1>
